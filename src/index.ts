0b2161559883f5bdd1e188ef2a5e14642d645408
Berkmann18 <maxieberkmann@gmail.com>
2019-07-20 15:37:49 +0100

    Fix: Lint fix

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Prando from 'prando';
 
 let RNG = new Prando();
 
-const shuffle = (a: any[]) => {
+const shuffle = (a: any[]): any[] => {
   for (let i: number = a.length - 1; i > 0; i--) {
     const j: number = Math.floor(RNG.next() * (i + 1));
     [a[i], a[j]] = [a[j], a[i]];
@@ -25,14 +25,15 @@ const trainValidationTestSplit = (
   validation: number,
   seed: number = -1.1,
   indices = false
-) => {
+): [any[], any[], any[]] => {
   let arr = [...data];
 
   if (seed !== -1.1) RNG = new Prando(seed);
 
   if (indices) {
     let i = 0;
-    arr = arr.map(el => i++); //@todo Change this
+    //eslint-disable-next-line @typescript-eslint/no-unused-vars
+    arr = arr.map((el: any): number => i++); //@todo Change this
   }
 
   // If train < 1, assume percentage (same thing for validation)
