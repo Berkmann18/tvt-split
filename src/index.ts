a6a9d49d5e492f506862a1327f8221e78a5d377d
Berkmann18 <maxieberkmann@gmail.com>
2019-07-20 13:05:13 +0100

    Update: Refactored + use `validation*`

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,37 @@
 import seedrandom from 'seedrandom';
 
-//@todo make use of validationSet
-//@todo refactor
 //@todo fix errors
 //@todo test
 //@todo publish
 
-const trainValidationTestSplit = (() => {
-  const shuffle = (a: any[]) => {
-    for (let i: number = a.length - 1; i > 0; i--) {
-      const j: number = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
-  };
+const shuffle = (a: any[]) => {
+  for (let i: number = a.length - 1; i > 0; i--) {
+    const j: number = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+};
 
-  return (data, train, validation, seed = null, indices = false) => {
-    let arr = [...data];
+const trainValidationTestSplit = (data: any[], train: number, validation: number, seed = null, indices = false) => {
+  let arr = [...data];
 
-    if (seed !== null) Math.seedrandom(seed);
+  if (seed !== null) Math.seedrandom(seed);
 
-    if (indices) {
-      let i = 0;
-      arr = arr.map(el => i++);
-    }
+  if (indices) {
+    let i = 0;
+    arr = arr.map(el => i++);
+  }
 
-    // If train < 1, assume percentage
-    if (train < 1) train = (train * arr.length) | 1;
+  // If train < 1, assume percentage (same thing for validation)
+  if (train < 1) train = Math.ceil(train * arr.length);
+  if (validation < 1) validation = Math.ceil(validation * arr.length);
 
-    const shuffled = shuffle([...arr]);
-    const testSet = shuffled.splice(0, arr.length - train);
-    const validationSet = shuffled.splice(0, arr.length - train - validation);
-    const trainSet = shuffled;
+  const shuffled = shuffle([...arr]);
+  const testSet = shuffled.splice(0, arr.length - train - validation);
+  const validationSet = shuffled.splice(0, shuffled.length - train);
+  const trainSet = shuffled;
 
-    return [trainSet, validationSet, testSet];
-  };
-})();
+  return [trainSet, validationSet, testSet];
+};
 
 export default trainValidationTestSplit;
